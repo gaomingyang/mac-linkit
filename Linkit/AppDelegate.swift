649fd2208016d082a39db870e38127577835f7a1
gaomingyang <gaomingyangcn@gmail.com>
2018-02-20 17:49:30 +0800

    Initial Commit

---
+++ Linkit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Linkit
+//
+//  Created by Sunshine on 20/02/2018.
+//  Copyright © 2018 Sunshine. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
